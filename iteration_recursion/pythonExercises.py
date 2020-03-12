108e2c008f632cd595f8d3e417e181be1a3f684b
Seth Villavicencio <SethVilla13@gmail.com>
2020-03-11 23:57:06 -0400

    complete exercises

--- iteration_recursion/pythonExercises.py
+++ iteration_recursion/pythonExercises.py
@@ -118,3 +118,15 @@ def countdown(n):
     print 'Blastoff!'
 
 countdown(7)
+
+#Exercises
+
+#3
+
+def check_fermat(a,b,c,n):
+  if(n > 2 and a**n + b**n == c**n):
+    print('Holy smokes, Fermat was wrong!')
+  else:
+    print('No, that doesn’t work.')
+
+check_fermat(3,4,5,3)
